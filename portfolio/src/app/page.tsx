99dfbcebce82ebe5f0115fdfb33f1cd2b49e75c9
Ben Allen <benjaminaallenjr@gmail.com>
2024-02-25 01:48:45 -0500

    updated effect

--- portfolio/src/app/page.tsx
+++ portfolio/src/app/page.tsx
@@ -1,9 +1,25 @@
 import { Sparkles } from './sparkles';
-
+import { TextGenerateEffectDemo } from './words';
 export default function page() {
 	return (
-		<div>
-			<Sparkles />
+		<div className="h-full flex flex-col lg:flex-row px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48 text-xl font-bold ">
+			{/* Image container */}
+			<div className="h-1/2 lg:h-full lg:w-1/2 ">
+				<TextGenerateEffectDemo />
+			</div>
+			{/* Text container */}
+			<div className=" lg:h-full lg:w-1/2 flex flex-col gap-8 items-center justify-center">
+				{/* Title */}
+				<Sparkles />
+				<div className="flex flex-row gap-5">
+					<button className="p-4 ring-1 ring-slate-400 rounded-lg bg-white text-black">
+						View My Work
+					</button>
+					<button className="p-4 ring-1 ring-blue-300 bg-white text-black rounded-lg">
+						Contact
+					</button>
+				</div>
+			</div>
 		</div>
 	);
 }
--- portfolio/src/app/sparkles.tsx
+++ portfolio/src/app/sparkles.tsx
@@ -1,10 +1,10 @@
 'use client';
 import React from 'react';
 import { SparklesCore } from '@/components/ui/sparkles';
-import { TextGenerateEffectDemo } from './words';
+
 export function Sparkles() {
 	return (
-		<div className="h-[40rem] relative w-full bg-black flex flex-col items-center justify-center overflow-hidden rounded-md">
+		<div className="h-[30rem] relative w-full bg-black flex flex-col items-center justify-center overflow-hidden rounded-md ">
 			<div className=" absolute inset-0 ">
 				<SparklesCore
 					id="tsparticlesfullpage"
@@ -15,25 +15,6 @@ export function Sparkles() {
 					className="w-full h-full"
 					particleColor="#FFFFFF"
 				/>
-			</div>
-			<div className="h-full flex flex-col lg:flex-row px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48 text-xl font-bold overflow-hidden">
-				{/* Image container */}
-				<div className="h-1/2 lg:h-full lg:w-1/2 ">
-					<TextGenerateEffectDemo />
-				</div>
-				{/* Text container */}
-				<div className="h-1/2 lg:h-full lg:w-1/2 flex flex-col gap-8 items-center justify-center">
-					{/* Title */}
-
-					<div className="flex flex-row gap-5 overflow-hidden">
-						<button className="p-4 ring-1 ring-slate-400 rounded-lg bg-white text-black">
-							View My Work
-						</button>
-						<button className="p-4 ring-1 ring-blue-300 bg-white text-black rounded-lg">
-							Contact
-						</button>
-					</div>
-				</div>
 			</div>
 		</div>
 	);
